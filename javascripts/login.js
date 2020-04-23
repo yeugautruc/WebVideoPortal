ac4ab904f24b8aa4789d27e13819722ee273cacb
yeugautruc <yeugautruc@gmail.com>
2020-04-23 19:20:56 +0200

    Update

--- javascripts/login.js
+++ javascripts/login.js
@@ -4,8 +4,22 @@ let loggedIn = false;
 
 
 function login() {
-console.log(document.getElementById('user-log-in').value);
-console.log(document.getElementById('pwd-log-in').value);
+    let usr = document.getElementById('user-log-in').value;
+    let pwd = document.getElementById('pwd-log-in').value
+    console.log(document.getElementById('user-log-in').value);
+    console.log(document.getElementById('pwd-log-in').value);
 
-   
+    fetch('json/userData.json').then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+        for (i in data) {
+        	console.log(data[i].username);
+        	console.log(data[i].password);
+            if (data[i].username == usr && data[i].password == pwd) {
+                loggedIn = true;
+            }
+        }
+    }).catch(err => {
+    });
 }
