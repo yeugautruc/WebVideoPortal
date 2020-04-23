a37ad2e4e4d50a412f9b086568b27ee2d9898008
yeugautruc <yeugautruc@gmail.com>
2020-04-24 00:17:37 +0200

    Update

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,24 +3,37 @@ let loggedIn = false;
 function login() {
     let usr = document.getElementById('user-log-in').value;
     let pwd = document.getElementById('pwd-log-in').value
-
-    fetch('json/userData.json').then(response => {
-        return response.json();
-    }).then(data => {
-        for (i in data) {
-            if (data[i].username == usr && data[i].password == pwd) {
-                loggedIn = true;
+    if (usr == "") {
+        alertHome("Please enter Username!");
+    } else
+    if (pwd == "") {
+        alertHome("Please enter Password!");
+    } else
+    if (usr == "" && pwd == "") {
+        alertHome("Please enter Username and Password!");
+    } else {
+        fetch('json/userData.json').then(response => {
+            return response.json();
+        }).then(data => {
+            for (i in data) {
+                if (data[i].username == usr && data[i].password == pwd) {
+                    loggedIn = true;
+                }
             }
-        }
-        if (loggedIn == true) {
-        	alertHome("You Are Logged In!");
-            document.getElementById('login-form-id').style.display = "none";
-            document.getElementById('logOut-btn-id').style.display = "block";
-        }
-    }).catch(err => {});
+            if (loggedIn == true) {
+                alertHome("You Are Logged In!");
+                document.getElementById('login-form-id').style.display = "none";
+                document.getElementById('logOut-btn-id').style.display = "block";
+            } else {
+                alertHome("Incorrect Password or Username!");
+            }
+        }).catch(err => {});
+    }
 }
 
 function logout() {
+    alertHome("Logged Out!");
+    document.getElementById('login-form-id').reset();
     document.getElementById('login-form-id').style.display = "block";
     document.getElementById('logOut-btn-id').style.display = "none";
     loggedIn = false;
