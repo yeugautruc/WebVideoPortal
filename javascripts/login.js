fb72d94f997c3b7375e8afab509a43e88b3d8f29
yeugautruc <yeugautruc@gmail.com>
2020-04-24 14:13:18 +0200

    Update

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,33 +3,35 @@ let loggedIn = false;
 function login() {
     let usr = document.getElementById('user-log-in').value;
     let pwd = document.getElementById('pwd-log-in').value
-    if (usr == "") {
-        alertHome("Please enter Username!");
-    } else
-    if (pwd == "") {
-        alertHome("Please enter Password!");
-    } else
+
     if (usr == "" && pwd == "") {
         alertHome("Please enter Username and Password!");
     } else {
-        fetch('json/userData.json').then(response => {
-            return response.json();
-        }).then(data => {
-            for (i in data) {
-                if (data[i].username == usr && data[i].password == pwd) {
-                    loggedIn = true;
+        if (usr == "") {
+            alertHome("Please enter Username!");
+        } else
+        if (pwd == "") {
+            alertHome("Please enter Password!");
+        } else {
+            fetch('json/userData.json').then(response => {
+                return response.json();
+            }).then(data => {
+                for (i in data) {
+                    if (data[i].username == usr && data[i].password == pwd) {
+                        loggedIn = true;
+                    }
                 }
-            }
-            if (loggedIn == true) {
-                alertHome("You Are Logged In!");
-                document.getElementById('login-form-id').style.display = "none";
-                document.getElementById('logOut-btn-id').style.display = "block";
-                document.getElementById('search-id').style.display = "block";
-                document.getElementById('cover-id').setAttribute('src','images/cover2.jpg');
-            } else {
-                alertHome("Incorrect Password or Username!");
-            }
-        }).catch(err => {});
+                if (loggedIn == true) {
+                    alertHome("You Are Logged In!");
+                    document.getElementById('login-form-id').style.display = "none";
+                    document.getElementById('logOut-btn-id').style.display = "block";
+                    document.getElementById('search-id').style.display = "block";
+                    document.getElementById('cover-id').setAttribute('src', 'images/cover2.jpg');
+                } else {
+                    alertHome("Incorrect Password or Username!");
+                }
+            }).catch(err => {});
+        }
     }
 }
 
@@ -40,17 +42,18 @@ function logout() {
     document.getElementById('logOut-btn-id').style.display = "none";
     document.getElementById('search-id').style.display = "none";
     loggedIn = false;
-    document.getElementById('cover-id').setAttribute('src','images/cover.jpg');
-            
+    document.getElementById('cover-id').setAttribute('src', 'images/cover.jpg');
+
 }
 
-function focusSignin(a){
+function focusSignin(a) {
     a.elements[0].style.background = "white";
     a.elements[1].style.background = "white";
     a.elements[0].style.opacity = "0.8";
     a.elements[1].style.opacity = "0.8";
 }
-function focusSigninOut(a){
+
+function focusSigninOut(a) {
     a.elements[0].style.background = "transparent";
     a.elements[1].style.background = "transparent";
 }
